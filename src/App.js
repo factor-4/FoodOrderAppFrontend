1e7dde0f0063e9c56cbcccf3afc49e6fa0c81fdb
factor-4 <snk.sojib@gmail.com>
2025-10-23 20:06:14 +0300

    profile-orderHistory-review page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import RegisterPage from "./components/auth/RegisterPage";
@@ -7,6 +7,12 @@ import HomePage from "./components/home_menu/HomePage";
 import CategoriesPage from "./components/home_menu/CategoryPage";
 import MenuPage from "./components/home_menu/MenuPage";
 import MenuDetailsPage from "./components/home_menu/MenuDetailsPage";
+import ProfilePage from "./components/profile_cart/ProfilePage";
+import UpdateProfilePage from "./components/profile_cart/UpdateProfilePage";
+import OrderHistoryPage from "./components/profile_cart/OrderHistoryPage";
+import { AdminRoute, CustomerRoute } from "./services/Guard";
+import LeaveReviewPage from "./components/profile_cart/LeaveReviewPage";
+
 
 
 
@@ -23,6 +29,17 @@ function App() {
               <Route path="/menu" element={<MenuPage />} />
               <Route path="/menu/:id" element={<MenuDetailsPage />} />
 
+              <Route path="/profile" element={<CustomerRoute element={<ProfilePage />} />} />
+
+              <Route path="/update" element={<CustomerRoute element={<UpdateProfilePage />} />} />
+
+              <Route path="/my-order-history" element={<CustomerRoute element={<OrderHistoryPage />} />} />
+
+              <Route path="/leave-review" element={<CustomerRoute element={<LeaveReviewPage />} />} />
+
+
+
+              <Route path="*" element={<Navigate to={"/home"} />} />
 
 
 
